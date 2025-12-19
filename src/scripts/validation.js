adcaca90f5541c2f4192cf24c972ee49fe4fd3d5
trydov1k <mdimon00@mail.ru>
2025-12-19 22:43:02 +0500

    Реализовал функцию toggleButtonState

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -10,23 +10,29 @@ const checkInputValidity = (formElement, inputElement) => {};  /* проверя
 выводит кастомное сообщение об ошибке: «Разрешены только латинские, кириллические буквы, знаки дефиса и пробелы». 
 Текст ошибки разместить в data-* атрибуте поля ввода. */
 
-const hasInvalidInput = () => {};  /* возвращает значение true, если хотя бы одно поле формы не прошло валидацию */
+const hasInvalidInput = (inputList) => {};  /* возвращает значение true, если хотя бы одно поле формы не прошло валидацию */
 
 const disableSubmitButton = () => {};  /* делает кнопку формы неактивной */
 
 const enableSubmitButton = () => {};  /* включает кнопку формы */
 
-const toggleButtonState = (inputList, inputElement) => {};  /* включает или отключает кнопку формы в зависимости от валидности всех полей. 
-Если хотя бы одно из полей не прошло валидацию, кнопка формы должна быть неактивной. Если оба поля прошли — активной */
+const toggleButtonState = (inputList, buttonElement, validationSettings) => {
+  const inactiveClass = validationSettings.inactiveButtonClass;  // Получаем класс, который присвоим кнопке, если форма невалидна
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(inactiveClass);
+  } else {
+    buttonElement.classList.remove(inactiveClass);
+  }
+};  /* включает или отключает кнопку формы в зависимости от валидности всех полей. */
 
-const setEventListeners = (formElement, inputSelector, submitButtonSelector) => {
-  const inputList = Array.from(formElement.querySelectorAll(inputSelector));  // Получаем список полей ввода текущей формы
-  const buttonElement = Array.from(formElement.querySelector(submitButtonSelector));  // Получаем кнопку submit текущей формы
+const setEventListeners = (formElement, validationSettings) => {
+  const inputList = Array.from(formElement.querySelectorAll(validationSettings.inputSelector));  // Получаем список полей ввода текущей формы
+  const buttonElement = Array.from(formElement.querySelector(validationSettings.submitButtonSelector));  // Получаем кнопку submit текущей формы
   toggleButtonState(inputList, buttonElement);  // Включаем/выключаем кнопку отправки формы
   inputList.array.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement);  // Проверяем валидность полей формы
-      toggleButtonState(inputList, buttonElement);  // Включаем/выключаем кнопку отправки формы
+      toggleButtonState(inputList, buttonElement, validationSettings);  // Включаем/выключаем кнопку отправки формы
     })
   });
 };  /* добавляет обработчики события input для всех полей формы. */
@@ -41,7 +47,7 @@ const enableValidation = (validationSettings) => {
         evt.preventDefault();  // Удаляем стандартное поведение браузера при отправке формы
       });
 
-      setEventListeners(formElement, validationSettings.inputSelector, validationSettings.submitButtonSelector)
+      setEventListeners(formElement, validationSettings)
     });
 };  /* отвечает за включение валидации всех форм. */
 
@@ -50,7 +56,7 @@ const enableValidation = (validationSettings) => {
   formSelector: ".popup__form",                      ✔
   inputSelector: ".popup__input",                    ✔
   submitButtonSelector: ".popup__button",            ✔
-  inactiveButtonClass: "popup__button_disabled",
+  inactiveButtonClass: "popup__button_disabled",     ✔
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
 };*/
